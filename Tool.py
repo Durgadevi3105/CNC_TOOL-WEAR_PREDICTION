ef8bdcc917a61b0214b258524c1dbabc1308e13b
Durgadevi Naveenkumar <durga.n3105@gmail.com>
2025-03-05 01:30:10 +0530

    Update Tool.py

--- Tool.py
+++ Tool.py
@@ -39,7 +39,7 @@ def set_bg_image(image_path):
     """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_bg_image("360_F_896565357_fmPjmJPX8vKdZ7FGodwE3F8pxkFEfuCX.jpg")
+set_bg_image("")
 
 # Custom CSS for Sidebar Styling with White, Black, and Blue
 sidebar_style = """
